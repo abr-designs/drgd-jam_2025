dae9f9e320a3a8618fb3f3e448af738688221850
Asus\limeonade <limeonade@hotmail.com>
2025-04-26 20:18:36 -0400

    Recipes and Loots Tables

---
+++ Assets/Scripts/Collectables/Crafting/CraftingRecipeSO.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Crafting Recipe", menuName = "Inventory/Crafting Recipe")]
+public class CraftingRecipeSO : ScriptableObject {
+
+    public new string name;
+    public List<ItemStack> inputItemStackList;
+    public List<ItemStack> outputItemStackList;
+    public string recipeName;
+
+}
---
+++ Assets/Scripts/Collectables/Crafting/CraftingSystem.cs
@@ -0,0 +1,89 @@
+using Audio;
+using Audio.SoundFX;
+using NaughtyAttributes;
+using UnityEngine;
+
+public class CraftingSystem : MonoBehaviour
+{
+    //InventorySystem inventorySystem;
+
+    private void Start() {
+        //inventorySystem = GetComponent<InventorySystem>();
+    }
+
+    public void TestCraftingSword(CraftingRecipeSO recipe) {
+        TryCraftingRecipe(recipe);
+
+        SFX.OBJECT_PICKUP.PlaySound();
+    }
+
+    private bool TryCraftingRecipe(CraftingRecipeSO recipe) {
+
+        if (recipe == null)
+            return false;
+
+        if (!CheckInventoryQuantityForRecipe(recipe)) {
+            Debug.LogWarning($"You do not have the required items to craft {recipe.recipeName}");
+            return false;
+        }
+
+        // remove used items
+        RemoveCraftingRecipeIngredients(recipe);
+
+        // add new item to inventory
+        AddCraftingRecipeOutput(recipe);
+
+        return true;
+
+    }
+
+    private bool CheckInventoryQuantityForRecipe(CraftingRecipeSO recipe) {
+
+        bool haveQuantity = true;
+
+        foreach (ItemStack itemStack in recipe.inputItemStackList) {
+            if (InventorySystem.Instance.GetCountOfItem(itemStack.itemSo) < itemStack.quantity) {
+                haveQuantity = false;
+                break;
+            }
+        }
+
+        return haveQuantity;
+    }
+
+    private void RemoveCraftingRecipeIngredients(CraftingRecipeSO recipe) {
+
+        foreach (ItemStack itemStack in recipe.inputItemStackList) {
+            InventorySystem.Instance.RemoveItem(itemStack);
+        }
+    }
+
+    private void AddCraftingRecipeOutput(CraftingRecipeSO recipe) {
+
+        foreach (ItemStack itemStack in recipe.outputItemStackList) {
+            InventorySystem.Instance.InsertItemStackToInventory(itemStack);
+        }
+    }
+
+    //Unity Editor
+    //============================================================================================================//
+
+#if UNITY_EDITOR
+
+    [SerializeField, Header("DEBUGGING")]
+    private bool requireApplicationPlaying = true;
+    [SerializeField] private CraftingRecipeSO debugCraftingRecipe;
+
+    [Button]
+    private void CraftGold()
+    {
+        if (requireApplicationPlaying && Application.isPlaying == false)
+            return;
+
+        Debug.Log("Craft Gold");
+        TryCraftingRecipe(debugCraftingRecipe);
+    }
+
+#endif
+    //============================================================================================================//
+}
