abb6fbe8d935c27179f124ed9e2f6f626a525d52
Alex Bedard-Reid <alexbedardreid@gmail.com>
2025-04-27 11:57:36 -0700

    Bug Fixes
    [FIX] Fixed issue with player death not stopping the spawning of Dynamite
    [FIX] Fixed AnimatorTransform issues

--- Assets/Scripts/UI/ButtonPressReactor.cs
+++ Assets/Scripts/UI/ButtonPressReactor.cs
@@ -1,3 +1,4 @@
+using System;
 using Audio;
 using Audio.SoundFX;
 using UnityEngine;
@@ -11,13 +12,28 @@ public class ButtonPressReactor : MonoBehaviour
     private SFX soundWhenPressed;
     private TransformAnimator m_transformAnimator;
     
+    private Vector3 m_startScale;
+    private Quaternion m_startRotation;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         var button = GetComponent<Button>();
         m_transformAnimator = GetComponent<TransformAnimator>();
         
+        m_startScale = transform.localScale;
+        m_startRotation = transform.localRotation;
+        
         button.onClick.AddListener(OnButtonPressed);
+    }
+
+    private void OnDisable()
+    {
+        m_transformAnimator.Stop();
+        
+        transform.localScale = m_startScale;
+        transform.localRotation = m_startRotation;
     }
 
 
