3b90952f8076ea4001f7c97f15a15754e8e8843e
CieroCeramics <red_wolf19@hotmail.com>
2025-04-26 21:35:54 -0700

    upgrades (#15)
    * upgrades
    
    upgrade recipes and implementation for capacity and health upgrades
    
    * added prefab
    
    added prefab
    
    * Update Upgrade Manager
    
    ---------
    
    Co-authored-by: Alex Bedard-Reid <alexbedardreid@gmail.com>

--- Assets/Scripts/Collectables/InventorySystem.cs
+++ Assets/Scripts/Collectables/InventorySystem.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
+using Interfaces;
 using UnityEngine;
 
 [Serializable]
-public class ItemStack {
+public class ItemStack
+{
     public InventoryItemSO itemSo;
     public int quantity;
 
-    public ItemStack(InventoryItemSO itemSo, int quantity) {
+    public ItemStack(InventoryItemSO itemSo, int quantity)
+    {
         this.itemSo = itemSo;
         this.quantity = quantity;
     }
@@ -18,19 +21,24 @@ public class ItemStack {
     }
 }
 
-public class InventorySystem : MonoBehaviour
+public class InventorySystem : MonoBehaviour, IHaveUpgrade
 {
     private static InventorySystem _instance;
     public static InventorySystem Instance { get { return _instance; } }
 
+    public float multiplier { get; private set; }
+
     private List<ItemStack> items = new List<ItemStack>();
+    int maxDistinctItems = 3;
 
     int currentSelectedIndex = 0;
 
     public static event Action<ItemStack> OnSelectNewItemStackAction;
 
-    private void Awake() {
-        if (_instance != null) {
+    private void Awake()
+    {
+        if (_instance != null)
+        {
             Destroy(gameObject);
             return;
         }
@@ -39,12 +47,15 @@ public class InventorySystem : MonoBehaviour
 
     }
 
-    public int GetCountOfItem(InventoryItemSO itemSo) {
+    public int GetCountOfItem(InventoryItemSO itemSo)
+    {
 
         int countOfItems = 0;
 
-        for (int i = 0; i < items.Count; i += 1) {
-            if (items[i].itemSo == itemSo) {
+        for (int i = 0; i < items.Count; i += 1)
+        {
+            if (items[i].itemSo == itemSo)
+            {
                 //itemStackIndex = i;
                 countOfItems += items[i].quantity;
                 break;
@@ -54,14 +65,18 @@ public class InventorySystem : MonoBehaviour
         return countOfItems;
     }
 
-    public void InsertItemStackToInventory(ItemStack newItemStack) {
-
+    public void InsertItemStackToInventory(ItemStack newItemStack)
+    {
+        if (items.Count > maxDistinctItems)
+            return;
         int itemStackIndex = -1;
-        for (int i = 0; i < items.Count; i += 1) {
+        for (int i = 0; i < items.Count; i += 1)
+        {
             if (items[i].itemSo == newItemStack.itemSo)
             {
                 // check current quantity of existing item stack
-                if (items[i].quantity + newItemStack.quantity > items[i].itemSo.maxHoldCount) {
+                if (items[i].quantity + newItemStack.quantity > items[i].itemSo.maxHoldCount)
+                {
                     itemStackIndex = -1;
                     break;
                 }
@@ -72,14 +87,20 @@ public class InventorySystem : MonoBehaviour
         }
 
         // increment quantity
-        if(itemStackIndex > -1) {
+        if (itemStackIndex > -1)
+        {
             items[itemStackIndex].quantity += newItemStack.quantity;
-        } else {
+        }
+        else
+        {
             // check if inventory is empty
-            if (items.Count > 0) {
+            if (items.Count > 0)
+            {
                 // insert as the next item
                 InsertNewItemStackToInventory(newItemStack, currentSelectedIndex + 1);
-            } else {
+            }
+            else
+            {
                 // inventory is empty, insert as first item
                 InsertNewItemStackToInventoryAtEnd(newItemStack);
             }
@@ -88,20 +109,24 @@ public class InventorySystem : MonoBehaviour
         //Debug.Log(ToString());
     }
 
-    public void InsertNewItemStackToInventory(ItemStack newItemStack, int index) {
+    public void InsertNewItemStackToInventory(ItemStack newItemStack, int index)
+    {
         // insert at index
         items.Insert(index, newItemStack);
     }
 
-    public static void TryInsertNewItemStackToInventoryAtEnd(ItemStack newItemStack) {
+    public static void TryInsertNewItemStackToInventoryAtEnd(ItemStack newItemStack)
+    {
         _instance.InsertNewItemStackToInventoryAtEnd(newItemStack);
     }
 
-    public void InsertNewItemStackToInventoryAtEnd(ItemStack newItemStack) {
+    public void InsertNewItemStackToInventoryAtEnd(ItemStack newItemStack)
+    {
         items.Add(newItemStack);
     }
 
-    public void RemoveItem(ItemStack itemStack) {
+    public void RemoveItem(ItemStack itemStack)
+    {
 
         InventoryItemSO itemSo = itemStack.itemSo;
         int quantity = itemStack.quantity;
@@ -109,26 +134,32 @@ public class InventorySystem : MonoBehaviour
         RemoveItem(itemSo, quantity);
     }
 
-    public void RemoveItem(InventoryItemSO itemSo, int quantity) {
+    public void RemoveItem(InventoryItemSO itemSo, int quantity)
+    {
 
         // check if item is in inventory
         int itemStackIndex = -1;
-        for (int i = 0; i < items.Count; i += 1) {
-            if (items[i].itemSo == itemSo) {
+        for (int i = 0; i < items.Count; i += 1)
+        {
+            if (items[i].itemSo == itemSo)
+            {
                 itemStackIndex = i;
                 break;
             }
         }
 
         // increment quantity
-        if (itemStackIndex > -1) {
+        if (itemStackIndex > -1)
+        {
             items[itemStackIndex].quantity -= quantity;
 
-            if(items[itemStackIndex].quantity <= 0) {
+            if (items[itemStackIndex].quantity <= 0)
+            {
                 // drop item from inventory
                 items.RemoveAt(itemStackIndex);
 
-                if(currentSelectedIndex >= items.Count) {
+                if (currentSelectedIndex >= items.Count)
+                {
                     currentSelectedIndex = items.Count - 1;
                 }
             }
@@ -145,13 +176,19 @@ public class InventorySystem : MonoBehaviour
     {
         string str = $"Inventory Contains: ";
 
-        for(int i = 0; i < items.Count; i += 1)
+        for (int i = 0; i < items.Count; i += 1)
         {
             str += items[i].ToString();
 
-            if(i < items.Count - 1) str += ", ";
+            if (i < items.Count - 1) str += ", ";
         }
 
         return str;
+    }
+
+    public void ApplyUpgrade(float newMultiplier)
+    {
+        multiplier = newMultiplier;
+        maxDistinctItems = 3 + (int)multiplier;
     }
 }
